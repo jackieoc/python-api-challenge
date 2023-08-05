a747c37a1040764f7f669c0a31ed497c32948fb5
Jackie <jackie.ochuida@gmail.com>
2023-08-05 12:30:17 -0400

    initial

---
+++ Starter_Code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "cb6513664804d6bc74081b042f8fd056"
+
+# Geoapify API Key
+geoapify_key = "199cb9e4393141cd8a92db4c7488d1e0"
---
+++ WeatherPy/api_keys.py
@@ -0,0 +1,2 @@
+# OpenWeatherMap API Key
+weather_api_key = "cb6513664804d6bc74081b042f8fd056"
